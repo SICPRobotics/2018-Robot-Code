773b4e980527cbb0f92b2ff76fc2b14168c0e9ca
sicp <sicp@10.58.22.37>
2018-01-16 16:59:24 -0600

    added cheesy drive and dead zone

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/subsystems/Drivetrain.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/subsystems/Drivetrain.java
@@ -9,9 +9,6 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- *
- */
 public class Drivetrain extends Subsystem 
 {
 	WPI_TalonSRX frontL,frontR,rearL,rearR;
@@ -35,8 +32,26 @@ public class Drivetrain extends Subsystem
     
     public void cheesyDrive(Joystick j)
     {
-    	//add dead zone
-    	robotBase.arcadeDrive(j.getY()*-1, j.getX()*-1, true);
+    	double scale = j.getRawAxis(3)*-1;
+		scale = ((scale+1)/5)+0.6; 
+		
+		double moveValue = j.getRawAxis(1);
+		double rotateValue = j.getRawAxis(0); 
+
+		//dead zone on y axis value
+		if (Math.abs(moveValue)<0.005)
+			moveValue = 0; 
+
+		//creates a dead zone on x axis value only if the y axis value is small 
+		if (Math.abs(rotateValue)<0.005 && Math.abs(moveValue)<0.1)
+			rotateValue = 0;
+
+		//scale down the values 
+		moveValue = moveValue*scale*-1; 
+		rotateValue = rotateValue*-0.6; //rotation scale factor used last year (2016), can change 
+		
+		robotBase.arcadeDrive(moveValue, rotateValue, true);
+
     }
 }
 
