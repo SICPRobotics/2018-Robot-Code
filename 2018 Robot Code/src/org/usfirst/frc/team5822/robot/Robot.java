cddf3f1a64614c03e36ac7edc69d7e759b98eb54
sicp <sicp@10.58.22.37>
2018-01-17 16:12:21 -0600

    newest

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/Robot.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/Robot.java
@@ -32,7 +32,7 @@ public class Robot extends TimedRobot
 	}
 
 	@Override
-	public void disabledInit() {}
+	public void disabledInit() {}                                 
 
 	@Override
 	public void disabledPeriodic() 
