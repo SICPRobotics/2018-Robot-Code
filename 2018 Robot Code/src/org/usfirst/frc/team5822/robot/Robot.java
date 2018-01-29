931a754df6e9cc057afc1ed79049bfd1c4f9329e
sicp <sicp@ROBOTICS-5363.ignatius.org>
2018-01-29 17:32:53 -0600

    newest

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/Robot.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/Robot.java
@@ -16,7 +16,7 @@ import org.usfirst.frc.team5822.robot.subsystems.*;
 public class Robot extends TimedRobot 
 {
 	public static OI oi;
-	//hi
+	//hi there
 	Command m_autonomousCommand;
 	SendableChooser<Command> chooseAutonomous = new SendableChooser<>();
 	public static Drivetrain driveTrain;
