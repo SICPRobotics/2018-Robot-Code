1475b87b7546a4d134494907a345ed5c67d3f476
Noah <noaboston8@gmail.com>
2018-02-01 22:58:43 -0600

    newest

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/commands/MovingSwitch.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/commands/MovingSwitch.java
@@ -13,7 +13,7 @@ public class MovingSwitch extends Command {
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() { 
+    protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
