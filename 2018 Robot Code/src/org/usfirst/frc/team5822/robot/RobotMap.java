e4367a45384f97382d36a4c828d35307b35a1070
Noah <noaboston8@gmail.com>
2018-02-15 09:05:11 -0600

    formatting

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/RobotMap.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/RobotMap.java
@@ -1,49 +1,47 @@
- package org.usfirst.frc.team5822.robot;
+package org.usfirst.frc.team5822.robot;
 
 public class RobotMap 
 {
-	//motors
-	public static int k_rearLeft = 1;
-	public static int k_frontLeft = 4;
+    //motors
+    public static int k_rearLeft = 1;
+    public static int k_frontLeft = 4;
 	
-	public static int k_frontRight = 2;
-	public static int k_rearRight = 3;
+    public static int k_frontRight = 2;
+    public static int k_rearRight = 3;
 	
+    public static int k_intakeLeft = 5;
+    public static int k_intakeRight = 6;
 	
-	public static int k_intakeLeft = 5;
-	public static int k_intakeRight = 6;
+    public static int k_armLeft = 7;
+    public static int k_armRight = 8;
+    public static int k_pot = 0;
+    
+    //not used yet
+    public static int k_climbing;
+    public static int k_elevatorLeft;
+    public static int k_elevatorRight;
 	
-	public static int k_armLeft = 7;
-	public static int k_armRight = 8;
-	public static int k_pot = 0;
-	//not used yet
-	public static int k_climbing;
-	public static int k_elevatorLeft;
-	public static int k_elevatorRight;
-	
-	//sensors
-	public static int k_leftEncoder_A = 0;
-	public static int k_leftEncoder_B = 1;
-	public static int k_rightEncoder_A = 2;
-	public static int k_rightEncoder_B = 3;
+    //sensors
+    public static int k_leftEncoder_A = 0;
+    public static int k_leftEncoder_B = 1;
+    public static int k_rightEncoder_A = 2;
+    public static int k_rightEncoder_B = 3;
 
-	public static int k_gyro = 2;
+    public static int k_gyro = 2;
 	
-	public static int k_usbCam1 = 0;
-	public static int k_usbCam2 = 1;
+    public static int k_usbCam1 = 0;
+    public static int k_usbCam2 = 1;
 	
-	//joysticks
-	public static int k_joystick1 = 0;
-	public static int k_xboxCntrl = 1;
+    //joysticks
+    public static int k_joystick1 = 0;
+    public static int k_xboxCntrl = 1;
 	
-	public static double k_switch = 0;
-	public static double k_scale = 1;
+    public static double k_switch = 0;
+    public static double k_scale = 1;
 	
-	//Arm height
-	public static double k_potentClimb;
-	public static double k_potentScale;
-	public static double k_potentSwitch;
-	public static double k_potentBottom;
-	
-	
+    //Arm height
+    public static double k_potentClimb;
+    public static double k_potentScale;
+    public static double k_potentSwitch;
+    public static double k_potentBottom;
 }
