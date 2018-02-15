9a615da0994103a552d4a8205e53c3408375a04b
Noah <noaboston8@gmail.com>
2018-02-15 09:01:12 -0600

    formatting

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/Robot.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/Robot.java
@@ -17,90 +17,84 @@ import org.usfirst.frc.team5822.robot.subsystems.*;
 
 public class Robot extends TimedRobot 
 {
-	public static OI oi;
+    public static OI oi;
 	
-	Command m_autonomousCommand;
-	SendableChooser<Command> chooseAutonomous = new SendableChooser<>();
+    Command m_autonomousCommand;
+    SendableChooser<Command> chooseAutonomous = new SendableChooser<>();
+    
+    public static Drivetrain driveTrain;
+    public static Sensors sensors;
+    public static IntakeArm intakeArm;
+    public static AntiFallMech antiFall;
 	
-	public static Drivetrain driveTrain;
-	public static Sensors sensors;
-	public static IntakeArm intakeArm;
-	public static AntiFallMech antiFall;
-	
-	public static AutoMode autoMode;
-	
-	
-	public static Joystick j = new Joystick(RobotMap.k_joystick1);
-	public String fieldDataIMP;
+    public static AutoMode autoMode;
 
-	int position = 1;
+    public static Joystick j = new Joystick(RobotMap.k_joystick1);
+    public String fieldDataIMP;
+
+    int position = 1;
 	
+    @Override
+    public void robotInit() 
+    {
+	driveTrain = new Drivetrain();
+	sensors = new Sensors();
+	intakeArm = new IntakeArm();
+	antiFall = new AntiFallMech();
 	
-	
-	@Override
-	public void robotInit() 
-	{
-		driveTrain = new Drivetrain();
-		sensors = new Sensors();
-		intakeArm = new IntakeArm();
-		antiFall = new AntiFallMech();
+	oi = new OI();
+    }
+    
+    @Override
+    public void disabledInit() {}                                 
+
+    @Override
+    public void disabledPeriodic() 
+    {
+	//Scheduler.getInstance().removeAll();
+    }
+
+    @Override
+    public void autonomousInit() 
+    {
+	Robot.sensors.resetGyro();
+	driveTrain.setPoint(0);
+	System.out.print("Set point" + driveTrain.getSetpoint());
+	Robot.sensors.getGyro();
+	fieldDataIMP = DriverStation.getInstance().getGameSpecificMessage(); //GETTING THE FMS DATA
 		
-		oi = new OI(); 
-	}
-
-	@Override
-	public void disabledInit() {}                                 
-
-	@Override
-	public void disabledPeriodic() 
-	{
-		//Scheduler.getInstance().removeAll();
-	}
-
-	@Override
-	public void autonomousInit() 
-	{
+	position = 1;
 		
-		Robot.sensors.resetGyro();
-		driveTrain.setPoint(0);
-		System.out.print("Set point" + driveTrain.getSetpoint());
-		Robot.sensors.getGyro();
-		fieldDataIMP = DriverStation.getInstance().getGameSpecificMessage(); //GETTING THE FMS DATA
-		
-		position = 1;
-		
-		m_autonomousCommand = new AutoMode(fieldDataIMP, position);
-		m_autonomousCommand.start();
-		
-	}
+	m_autonomousCommand = new AutoMode(fieldDataIMP, position);
+	m_autonomousCommand.start();
+    }
 
-	@Override
-	public void autonomousPeriodic() 
+    @Override
+    public void autonomousPeriodic() 
+    {
+	Scheduler.getInstance().run();
+	System.out.println("Drivetrain encoders in subsystem: " + driveTrain.encDistance());
+    }
+    
+    @Override
+    public void teleopInit() 
+    {
+	if (m_autonomousCommand != null) 
 	{
-		Scheduler.getInstance().run();
-		System.out.println("Drivetrain encoders in subsystem: " + driveTrain.encDistance());
+	    m_autonomousCommand.cancel();
 	}
+	Robot.driveTrain.disable(); //disable any PIDs that were running
+	Robot.sensors.resetEncoders();
+    }
 
-	@Override
-	public void teleopInit() 
-	{
-		if (m_autonomousCommand != null) 
-		{
-			m_autonomousCommand.cancel();
-		}
-		Robot.driveTrain.disable(); //disable any PIDs that were running
-		Robot.sensors.resetEncoders();
-		
-	}
+    @Override
+    public void teleopPeriodic() 
+    {
+	Scheduler.getInstance().run();
+	driveTrain.cheesyDrive(j);
+	System.out.println("pot: " + Robot.sensors.getPot());
+    }
 
-	@Override
-	public void teleopPeriodic() 
-	{
-		Scheduler.getInstance().run();
-		driveTrain.cheesyDrive(j);
-		System.out.println("pot: " + Robot.sensors.getPot());
-	}
-
-	@Override
-	public void testPeriodic() {}
+    @Override
+    public void testPeriodic() {}
 }
