b240907b2dfb302c664523aeff9d8dc42a831f65
Noah <noaboston8@gmail.com>
2018-02-15 09:03:19 -0600

    formatting

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/OI.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/OI.java
@@ -9,47 +9,45 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 //hi there
 public class OI 
 {
+    Joystick j = new Joystick(RobotMap.k_joystick1);
+    Joystick x = new Joystick(RobotMap.k_xboxCntrl);
 	
-	Joystick j = new Joystick(RobotMap.k_joystick1);
-	Joystick x = new Joystick(RobotMap.k_xboxCntrl);
+    Button j3 = new JoystickButton(Robot.j, 3);
+    Button j4 = new JoystickButton(Robot.j, 4);		
+    Button j5 = new JoystickButton(Robot.j, 5);
+    Button j6 = new JoystickButton(Robot.j, 6);
+    Button j7 = new JoystickButton(Robot.j, 7);
+    Button j8 = new JoystickButton(Robot.j, 8);
 	
-	Button j3 = new JoystickButton(Robot.j, 3);
-	Button j4 = new JoystickButton(Robot.j, 4);		
-	Button j5 = new JoystickButton(Robot.j, 5);
-	Button j6 = new JoystickButton(Robot.j, 6);
-	Button j7 = new JoystickButton(Robot.j, 7);
-	Button j8 = new JoystickButton(Robot.j, 8);
-	
-	//9 and 10 don't work on our joystick 
-	Button j11 = new JoystickButton(Robot.j, 11); 
-	Button j12 = new JoystickButton(Robot.j, 12);
+    //9 and 10 don't work on our joystick 
+    Button j11 = new JoystickButton(Robot.j, 11); 
+    Button j12 = new JoystickButton(Robot.j, 12);
 
-	Button buttonA = new JoystickButton(x, 1);
-	Button buttonB = new JoystickButton(x, 2);
-	Button buttonX = new JoystickButton(x, 3);
-	Button buttonY = new JoystickButton(x, 4);
-	Button buttonLB = new JoystickButton(x, 5);
-	Button buttonRB = new JoystickButton(x, 6);
-	Button buttonBack = new JoystickButton(x, 7);
-	Button buttonStart = new JoystickButton(x, 8);	
+    Button buttonA = new JoystickButton(x, 1);
+    Button buttonB = new JoystickButton(x, 2);
+    Button buttonX = new JoystickButton(x, 3);
+    Button buttonY = new JoystickButton(x, 4);
+    Button buttonLB = new JoystickButton(x, 5);
+    Button buttonRB = new JoystickButton(x, 6);
+    Button buttonBack = new JoystickButton(x, 7);
+    Button buttonStart = new JoystickButton(x, 8);	
 	
-	public OI()
-	{
-		//RumbleType[] kleftRumble = GenericHID.RumbleType.values();
-		//x.setRumble(kleftRumble[1], 1.0);
-		//buttonX.whenPressed(new MoveArm(1500));
+    public OI()
+    {
+	//RumbleType[] kleftRumble = GenericHID.RumbleType.values();
+	//x.setRumble(kleftRumble[1], 1.0);
+	//buttonX.whenPressed(new MoveArm(1500));
 		
-		buttonA.whenPressed(new Intake(-.60, false)); // negative out B
-		buttonA.whenReleased(new Intake(0, false));
+	buttonA.whenPressed(new Intake(-.60, false)); // negative out B
+	buttonA.whenReleased(new Intake(0, false));
 		
-		buttonB.whenPressed(new Intake(.60, false)); // positive in A
-		buttonB.whenReleased(new Intake(0, false));
+	buttonB.whenPressed(new Intake(.60, false)); // positive in 
+	buttonB.whenReleased(new Intake(0, false));
 		
-		buttonY.whenPressed(new Intake(-.60, true));
-		buttonY.whenReleased(new Intake(0, false));
+	buttonY.whenPressed(new Intake(-.60, true));
+	buttonY.whenReleased(new Intake(0, false));
 		
-		
-		buttonStart.whenActive(new SolenoidForward());
-		buttonBack.whenActive(new SoloReverse());
-	}
+	buttonStart.whenActive(new SolenoidForward());
+	buttonBack.whenActive(new SoloReverse());
+    }
 }
