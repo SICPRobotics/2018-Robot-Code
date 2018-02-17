84baa31991811c218c49f6bc72e9e738e118c780
sicp <sicp@ROBOTICS-5359.ignatius.org>
2018-02-16 18:49:56 -0600

    adjusted OI

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/OI.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/OI.java
@@ -19,19 +19,18 @@ public class OI
 	Button j5 = new JoystickButton(Robot.j, 5);
 	Button j6 = new JoystickButton(Robot.j, 6);
 	Button j7 = new JoystickButton(Robot.j, 7);
-	Button j8 = new JoystickButton(Robot.j, 8);
-	//9 and 11 don't work on our joystick 
+	Button j8 = new JoystickButton(Robot.j, 8); //9 and 11 don't work on our joystick 
 	Button j10 = new JoystickButton(Robot.j, 10); 
 	Button j12 = new JoystickButton(Robot.j, 12);
 
 	Button buttonA = new JoystickButton(x, 1); //Old 2
 	Button buttonB = new JoystickButton(x, 2); //Old 3
 	Button buttonX = new JoystickButton(x, 3); //Old 1
-	Button buttonY = new JoystickButton(x, 4); //Old 4
+	Button buttonY = new JoystickButton(x, 4); 
 	Button buttonLB = new JoystickButton(x, 5);
 	Button buttonRB = new JoystickButton(x, 6); 
 	Button buttonBack = new JoystickButton(x, 7); //Old 9
-	Button buttonStart = new JoystickButton(x, 8);	//Old 10 
+	Button buttonStart = new JoystickButton(x, 8); //Old 10 
 	Button leftThumb = new JoystickButton(x, 9);
 	Button rightThumb = new JoystickButton(x, 10);
 
