0bcb16059ed01bbe4f645f6cc529414b6f5945f1
sicp <sicp@ROBOTICS-5359.ignatius.org>
2018-02-16 23:32:21 -0600

    fixed up auto mode

--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/commands/A_Center.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/commands/A_Center.java
@@ -27,7 +27,7 @@ public class A_Center extends CommandGroup
     		addSequential(new DriveForward(60.785));
     		addSequential(new Intake(-.7));
     	} 
-    	else if (gameData.charAt(1) == 'R') 
+    	else if (gameData.charAt(0) == 'R') 
     	{
     		// Right Side Switch
     		System.out.println("Center Position/Right Switch");
@@ -38,6 +38,15 @@ public class A_Center extends CommandGroup
     		addSequential(new TurnGroup(-90));
     		addSequential(new DriveForward(60.785));
     		addSequential(new Intake(-.7));
+    	} else {
+    		//cross baseline - this should NOT happen
+    		System.out.println("Center Position/Cross Baseline");
+    		addSequential(new DriveForward(40.875));
+    		addSequential(new TurnGroup(90));
+    		addSequential(new DriveForward(46.2));
+    		addSequential(new TurnGroup(-90));
+    		addSequential(new DriveForward(60.785));
+
     	}
     	
     }
--- 2018 Robot Code/src/org/usfirst/frc/team5822/robot/commands/A_Right.java
+++ 2018 Robot Code/src/org/usfirst/frc/team5822/robot/commands/A_Right.java
@@ -11,7 +11,7 @@ public class A_Right extends CommandGroup
     	if (gameData.charAt(0) == 'R' && gameData.charAt(1) != 'R')
     	{
     		//Right Side Switch
-    		System.out.println("Right Position/Right Switch");
+    		System.out.println("Right Position/ R !R / Right Switch");
     		addSequential(new DriveForward(148.875));
     		addParallel(new MoveArm(RobotMap.k_potSwitch));
     		addSequential(new TurnGroup(-90));
@@ -21,19 +21,20 @@ public class A_Right extends CommandGroup
     	else if (gameData.charAt(0) != 'R' && gameData.charAt(1) == 'R')
     	{
     		// Right Side Scale
-			System.out.println("Right Position/Right Scale");
+			System.out.println("Right Position/ !R R/ Right Scale");
 			addSequential(new DriveForward(304.875));
 			addParallel(new MoveArm(RobotMap.k_potScale));
 			addSequential(new TurnGroup(-90));
 			addSequential(new DriveForward(5.755));
 			addSequential(new Intake(-.7));
     	}
-    	else if (gameData.charAt(0) == 'L' && gameData.charAt(1) == 'L')
+    	else if (gameData.charAt(0) == 'R' && gameData.charAt(1) == 'R')
     	{
+			System.out.println("Right Position/R R");
     		if (side == 0)
     		{
         		//Right Side Switch
-        		System.out.println("Right Position/Right Switch");
+        		System.out.println("User Chose Switch");
         		addSequential(new DriveForward(148.875));
         		addParallel(new MoveArm(RobotMap.k_potSwitch));
         		addSequential(new TurnGroup(-90));
@@ -43,7 +44,7 @@ public class A_Right extends CommandGroup
     		else if (side == 1)
     		{
         		// Right Side Scale
-    			System.out.println("Right Position/Right Scale");
+    			System.out.println("User Chose Scale");
     			addSequential(new DriveForward(304.875));
     			addParallel(new MoveArm(RobotMap.k_potScale));
     			addSequential(new TurnGroup(-90));
@@ -54,7 +55,7 @@ public class A_Right extends CommandGroup
     	else 
     	{
     		// Cross Baseline
-    		System.out.println("Left Position/Cross Baseline");
+    		System.out.println("Right Position/!R !R /Cross Baseline");
     		addSequential(new DriveForward(148.875));
     	}
     }
